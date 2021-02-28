8d810b773cc12060360b92f4717df022c2062a3b
Andrey Rakhmanov <rakhmanov.andrey@gmail.com>
2021-02-28 20:09:34 +0300

    Перезаписываю маску

--- hero_all/hero_class.py
+++ hero_all/hero_class.py
@@ -59,6 +59,7 @@ class Hero(pygame.sprite.Sprite):
         radius, angle = direction.as_polar()
         self.image = pg.transform.rotate(self.orig, -angle - 90)
         self.rect = self.image.get_rect(center=self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def fire(self, mouse_xy):
         if pygame.mouse.get_pressed(num_buttons=3)[0]:
